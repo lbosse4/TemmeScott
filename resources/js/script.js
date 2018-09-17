4cdb9cd7929af2c796e945551d5601af7f95383a
Lauren Bosse <lauren.bosse@viasat.com>
2018-09-16 20:27:03 -0700

    Added masonry plugin for photos layout

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,6 +1,8 @@
 $(document).ready(function() {
     
-    $('.grid').masonry({
+    var $container = jQuery('#masonry-grid');
+    
+    $container.masonry({
       // options
       itemSelector: '.grid-item',
       columnWidth: 200
